276ffb5720852e3098d59ac7504796d7220a87d6
Farid <faridmurzone@gmail.com>
2021-12-19 11:24:14 -0300

    Basic Types - Inference

---
+++ BasicTypes/main.rs
@@ -0,0 +1,15 @@
+#[allow(warnings)]
+
+fn main() {
+    // Type Inference
+    let some_boolean: bool = False;
+    let some_number: i32 = 5;
+    let some_string: &str = "holi",
+    let names_arr: [&str; 3] = ["Alice", "Bob", "Doe"]
+    let number: String = string_number.parse().unwrap();
+    let some_boolean_inference = False;
+    let some_number_inference = 5;
+    let some_string_inference = "holi",
+    let names_arr_inference = ["Alice", "Bob", "Doe"]
+    let number: String = string_number_inference.parse::<i32>().unwrap(); // Turbofish syntax
+}
